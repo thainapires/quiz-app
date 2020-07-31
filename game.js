d714140c2d1442c03f8a0f94fcb1fb0eb488ddf9
Thainá Pires <thainaspiress@gmail.com>
2020-07-31 00:21:50 -0300

    Loading questions from open trivia db API with fetch API

--- game.js
+++ game.js
@@ -11,11 +11,36 @@ let availableQuestions = {};
 
 let questions = [];
 
-fetch("questions.json")
+/*fetch("questions.json")
 .then( res => {
     return res.json();
 }).then(loadedQuestions => {
     questions = loadedQuestions;
+    startGame();
+})
+.catch( err => {
+    console.error(err);
+});*/
+
+fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
+.then( res => {
+    return res.json();
+}).then(loadedQuestions => {
+    questions = loadedQuestions.results.map( loadedQuestion =>{
+        const formattedQuestion = {
+            question: loadedQuestion.question
+        };
+
+        const answerChoices = [ ... loadedQuestion.incorrect_answers];
+        formattedQuestion.answer = Math.floor(Math.random()+3) +1;
+        answerChoices.splice(formattedQuestion.answer -1, 0, loadedQuestion.correct_answer);
+        answerChoices.forEach((choice, index) => {
+            formattedQuestion["choice" + (index+1)] = choice;
+        });
+
+        return formattedQuestion;
+    });
+    //questions = loadedQuestions;
     startGame();
 })
 .catch( err => {
