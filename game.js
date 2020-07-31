95413c5e0b662faefd30a8b662ee3da44a6a0cf5
Thainá Pires <thainaspiress@gmail.com>
2020-07-30 22:35:21 -0300

    Creating heads up display (HUD)

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -52,6 +54,7 @@ getNewQuestion = () => {
         return window.location.assign("/end.html");
     }
     questionCounter++;
+    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -72,6 +75,10 @@ choices.forEach(choice =>{
         const selectedAnswer = selectedChoice.dataset["number"];
         const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
+        if(classToApply == 'correct'){
+            incrementScore(CORRECT_BONUS);
+        }
+
         selectedChoice.parentElement.classList.add(classToApply);
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply);
@@ -80,4 +87,8 @@ choices.forEach(choice =>{
     });
 });
 
+incrementScore = num => {
+    score +=num;
+    scoreText.innerText = score;
+}
 startGame();
